03e59b6c0ac75365775fe50eea73bd5f460f05df
Lucas <lucas.nepomuceno1999@gmail.com>
2025-06-01 19:32:53 -0300

    criando projeto

---
+++ src/main/java/com/faj/lucas/santos/aula8restauranteserver/Aula8RestauranteServerApplication.java
@@ -0,0 +1,13 @@
+package com.faj.lucas.santos.aula8restauranteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula8RestauranteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula8RestauranteServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faj/lucas/santos/aula8restauranteserver/Aula8RestauranteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faj.lucas.santos.aula8restauranteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula8RestauranteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
